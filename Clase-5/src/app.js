bf04a7668fa7868efc193c519e45144643569ffa
Daniel Villajuan <danielvillajuan@hotmail.com>
2024-10-31 19:35:22 -0300

    clase 5 - inico

---
+++ Clase-5/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express'
+
+
+// const app = express()
+
+// app.get('/operacionsimple', (req, res) => {
+//     let sum = 0;
+//     for (let index = 0; index < 1000000; index++) {
+//         sum+=index        
+//     }
+//     res.json({ sum })
+// })
+
+// app.get('/operacioncompleja', (req, res) => {
+//     let sum = 0;
+//     for (let index = 0; index < 5e8; index++) {
+//         sum+=index        
+//     }
+//     res.json({ sum })
+// })
+
+// app.listen(8080, () => {
+//     console.log('Server en 8080')
+// })
